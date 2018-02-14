21ecac2a6527d31f03f3a656b70cb8b941e1b73f
AntoinePassemiers <apassemi@ulb.ac.be>
2018-02-14 21:56:51 +0100

    Bug fix: digits as tokens

--- src/examples/example.py
+++ src/examples/example.py
@@ -14,7 +14,7 @@ def moving_average(sequence, n=1000):
 
 
 # https://archive.org/stream/thusspokezarathu00nietuoft/thusspokezarathu00nietuoft_djvu.txt
-with open('../data/thus_spoke_zarathustra.txt', 'r') as f:
+with open('../../data/thus_spoke_zarathustra.txt', 'r') as f:
     text = f.read()
     lexicon = lbsa.create_lexicon(language='english')
     features = lbsa.time_analysis(text, lexicon)
@@ -27,16 +27,19 @@ with open('../data/thus_spoke_zarathustra.txt', 'r') as f:
     new_length = len(polarity) - (len(polarity) % block_size)
     polarity = np.mean(polarity[:new_length].reshape(-1, block_size), axis=1)
     polarity = moving_average(polarity, n=50)
-
     plt.plot(polarity)
-    plt.show()
 
+    fig = plt.gcf()
+    fig.set_size_inches(18.5, 10.5)
+    fig.savefig('polarity.png', dpi=100)
 
-    for feature_name in lbsa.SENTIMENT_NAMES[2:]:
+    n_sentiments = lexicon.get_n_sentiments()
+    sentiment_names = lexicon.get_sentiment_names()
+    for feature_name in sentiment_names[2:]:
         feature = features[feature_name]
         new_length = len(feature) - (len(feature) % block_size)
         feature = np.mean(feature[:new_length].reshape(-1, block_size), axis=1)
-        feature = moving_average(feature, n=80)
+        feature = moving_average(feature, n=100)
 
         plt.plot(feature, label=feature_name)
 
--- src/lbsa.py
+++ src/lbsa.py
@@ -12,7 +12,6 @@ import csv
 
 from nltk.tokenize import word_tokenize
 
-
 try: # Python 3
     from urllib.request import urlretrieve
 except ImportError: # Python 2
@@ -20,86 +19,101 @@ except ImportError: # Python 2
 
 
 home = os.path.expanduser("~")
-download_dir = os.path.join(home, 'lbsa_data')
-if not os.path.isdir(download_dir):
-    os.makedirs(download_dir)
+LBSA_DATA_DIR = os.path.join(home, 'lbsa_data')
+if not os.path.isdir(LBSA_DATA_DIR):
+    os.makedirs(LBSA_DATA_DIR)
 
-nrc_filename = "NRC-Emotion-Lexicon-v0.92-InManyLanguages-web"
-if not os.path.exists(os.path.join(download_dir, "%s.csv" % nrc_filename)):
-    # Download lexicon in XLSX format
-    if not os.path.exists(os.path.join(download_dir, "%s.xlsx" % nrc_filename)):
-        LEXICON_URL = "http://www.saifmohammad.com/WebDocs/%s.xlsx" % nrc_filename
-        urlretrieve(LEXICON_URL, os.path.join(download_dir, "%s.xlsx" % nrc_filename))
 
-    # Convert from XLSX to CSV file
-    wb = xlrd.open_workbook(os.path.join(download_dir, "%s.xlsx" % nrc_filename))
-    sheet = wb.sheet_by_name('NRC-Emotion-Lexicon-v0.92-InMan')
-    with open(os.path.join(download_dir, "%s.csv" % nrc_filename), mode='w', encoding='utf8') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for rownum in range(sheet.nrows):
-            writer.writerow(sheet.row_values(rownum))
+class Lexicon:
+
+    def __init__(self, dataframe, sentiment_names, language='english'):
+        self.dataframe = dataframe
+        self.sentiment_names = sentiment_names
+        self.language = language
+        self.table = dict()
+        self.set_language(self.table, language)
+
+    def set_language(self, table, language):
+        sentiments = np.asarray(self.dataframe[self.sentiment_names])
+        for key, value in zip(self.dataframe[self.language], sentiments):
+            if value.sum() > 0:
+                table[key] = value
     
-    # Remove XLSX file
-    os.remove(os.path.join(download_dir, "%s.xlsx" % nrc_filename))
-
-
-SENTIMENT_NAMES = ["positive", "negative", "anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
-
-LEXICON_PATH = os.path.join(__file__, os.path.join(download_dir, '%s.csv' % nrc_filename))
-LEXICON_ALL_LANGUAGES = pd.read_csv(LEXICON_PATH, encoding='utf8')
-LEXICON_ALL_LANGUAGES.rename(columns=lambda x: x.replace('Word', '').split('Translation')[0].rstrip(' ').lower(), inplace=True)
-for column_name in SENTIMENT_NAMES:
-    LEXICON_ALL_LANGUAGES[column_name] = LEXICON_ALL_LANGUAGES[column_name].astype(np.int32)
-
-
-class Analysis:
-    def __init__(self, values, n_words):
-        self.opinion = {
-            'positive': values[0] / n_words,
-            'negative': values[1] / n_words,
-        }
-
-        self.sentiments = {
-            name: values[i] for i, name in zip(range(2, len(SENTIMENT_NAMES)), SENTIMENT_NAMES[2:])
-        }
-        self.values = values
-
-    def __repr__(self):
-        return str({key: value for key, value in zip(SENTIMENT_NAMES, self.values)})
+    def get(self, token):
+        if token.isdigit():
+            return None
+        return self.table.get(token)
     
-    def __str__(self):
-        return self.__repr__()
+    def get_n_sentiments(self):
+        return len(self.sentiment_names)
+    
+    def get_sentiment_names(self):
+        return self.sentiment_names
+    
+    def get_analysis(self, counters):
+        return {
+            name: counter for name, counter in zip(self.sentiment_names, counters)
+        }
+    
+    def __len__(self):
+        return len(self.dataframe)
+
+
+def load_nrc_lexicon():
+    nrc_filename = "NRC-Emotion-Lexicon-v0.92-InManyLanguages-web"
+    if not os.path.exists(os.path.join(LBSA_DATA_DIR, "%s.csv" % nrc_filename)):
+        # Download lexicon in XLSX format
+        if not os.path.exists(os.path.join(LBSA_DATA_DIR, "%s.xlsx" % nrc_filename)):
+            LEXICON_URL = "http://www.saifmohammad.com/WebDocs/%s.xlsx" % nrc_filename
+            urlretrieve(LEXICON_URL, os.path.join(LBSA_DATA_DIR, "%s.xlsx" % nrc_filename))
+
+        # Convert from XLSX to CSV file
+        wb = xlrd.open_workbook(os.path.join(LBSA_DATA_DIR, "%s.xlsx" % nrc_filename))
+        sheet = wb.sheet_by_name('NRC-Emotion-Lexicon-v0.92-InMan')
+        with open(os.path.join(LBSA_DATA_DIR, "%s.csv" % nrc_filename), mode='w', encoding='utf8') as csv_file:
+            writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+            for rownum in range(sheet.nrows):
+                writer.writerow(sheet.row_values(rownum))
+        
+        # Remove XLSX file
+        os.remove(os.path.join(LBSA_DATA_DIR, "%s.xlsx" % nrc_filename))
+
+    sentiment_names = ["positive", "negative", "anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust"]
+    lexicon_path = os.path.join(__file__, os.path.join(LBSA_DATA_DIR, '%s.csv' % nrc_filename))
+    nrc_all_languages = pd.read_csv(lexicon_path, encoding='utf8')
+    nrc_all_languages.rename(columns=lambda x: x.replace('Word', '').split('Translation')[0].rstrip(' ').lower(), inplace=True)
+    for column_name in sentiment_names:
+        nrc_all_languages[column_name] = nrc_all_languages[column_name].astype(np.int32)
+    return nrc_all_languages, sentiment_names
 
 
 def create_lexicon(language='english'):
-    sentiments = np.asarray(LEXICON_ALL_LANGUAGES[SENTIMENT_NAMES])
-
-    lexicon = dict()
-    for key, value in zip(LEXICON_ALL_LANGUAGES[language], sentiments):
-        if value.sum() > 0:
-            lexicon[key] = value
+    nrc_all_languages, sentiment_names = load_nrc_lexicon()
+    lexicon = Lexicon(nrc_all_languages, sentiment_names, language=language)
     return lexicon
 
 
 def sentiment_analysis(text, lexicon):
     tokens = word_tokenize(text)
-    counters = np.zeros(len(SENTIMENT_NAMES), dtype=np.int)
+    n_sentiments = lexicon.get_n_sentiments()
+    counters = np.zeros(n_sentiments, dtype=np.int)
     for token in tokens:
         value = lexicon.get(token.lower())
         if value is not None:
             counters += value
-    return Analysis(counters, len(tokens))
+    return lexicon.get_analysis(counters)
 
 
 def time_analysis(text, lexicon):
     tokens = word_tokenize(text)
-    mask = np.zeros((len(tokens), len(SENTIMENT_NAMES)), dtype=np.int)
+    n_sentiments = lexicon.get_n_sentiments()
+    sentiment_names = lexicon.get_sentiment_names()
+    mask = np.zeros((len(tokens), n_sentiments), dtype=np.int)
     for token_id, token in enumerate(tokens):
         value = lexicon.get(token.lower())
         if value is not None:
             mask[token_id, :] += value
-        print('%i/%i' % (token_id+1, len(tokens)))
     features = {
-        key: value for key, value in zip(SENTIMENT_NAMES, mask.T)
+        key: value for key, value in zip(sentiment_names, mask.T)
     }
     return features
